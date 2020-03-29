f8dad4a4263c7fe4553868116fd00dd7964520da
Ben <bkilian@bu.edu>
2020-03-29 02:17:25 -0400

    this shit fire

--- src/game_loop.py
+++ src/game_loop.py
@@ -1,5 +1,5 @@
 import numpy as npo
-from lamp import lamp
+from lamp import lamp,Food
 import pygame
 import time
 
@@ -14,8 +14,8 @@ green = (0, 255, 0)
 blue = (0, 0, 128) 
 red = (255, 0, 0)
 # number of lamps in our colony
-numLamps = 2000
-
+numLamps = 250
+numFoods = 50
 # sets the title for the window
 pygame.display.set_caption('Environment') 
 
@@ -27,7 +27,31 @@ def renderLamp(lamp, display):
         lamp - the lamp to be rendered
         display - the display on which to render our lamp
         """
-        pygame.draw.rect(display, lamp.color, (lamp.x, lamp.y, 9,3), 0)
+        l = lamp.length
+        h = lamp.height
+        pygame.draw.rect(display, lamp.color, (lamp.position[0], lamp.position[1], l, h), 0)
+
+def renderFood(food, display):
+        h = food.height
+        l = food.length
+        pygame.draw.rect(display, blue, (food.position[0], food.position[1], l,h), 0)
+
+def isCollision(obj1, obj2):
+    (x1, y1) = obj1.position
+    (x2, y2) = obj2.position
+    if(x1 >= x2 and x1 <= x2+obj2.length) or (x1+obj1.length >= x2 and x1+obj1.length <= x2+obj2.length):
+        if(y1 >= y2 and y1 <= y2+obj2.height) or (y1+obj1.height >= y2 and y1+obj1.height <= y2+obj2.height):
+            return True
+    return False
+
+def endGame(display_):
+    display_.fill([255,255,255])
+    myfont = pygame.font.SysFont('Comic Sans MS', 50)
+    textsurface = myfont.render('LAMPS EXTINCT, GG.', False, (0, 0, 0))
+    display_.blit(textsurface,(boxWidth/4,boxHeight/2))
+    pygame.display.update()
+    time.sleep(3)
+
 
 def main():
 
@@ -45,26 +69,44 @@ def main():
         lamp_colony.append(lamp_i)
         renderLamp(lamp_colony[i], screen)
 
+    food_colony = []
+    for j in range(numFoods):
+        food_i = Food(10, 10)
+        food_colony.append(food_i)
+        renderFood(food_colony[j], screen)
+        
     running = True
-    
     while running:
         # reset screen color to white
         screen.fill([255,255,255])
-
-        # iterate through lamps, move them, and plot them
+        
+        dead_lamps = []
         for i in range(len(lamp_colony)):
             lamp_colony[i].move()
             renderLamp(lamp_colony[i], screen)
-            
+            for j in range(len(food_colony)):
+                    renderFood(food_colony[j], screen)
+                    if isCollision(lamp_colony[i], food_colony[j]):
+                            food_colony[j].respawn()
+                            lamp_colony[i].energy += 50
+            if lamp_colony[i].energy <= 0:
+                dead_lamps.append(lamp_colony[i])
+                
+        for lamp_ in dead_lamps:
+                lamp_colony.remove(lamp_)
+
         # if we click 'X' on the screen, stop rendering the screen
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 running = False
 
-        
+        if len(lamp_colony) == 0:
+            endGame(screen)
+            running = False            
         pygame.display.update()
         # pause the window so the graphics update
-        time.sleep(timeStep)
+#        time.sleep(timeStep/8)
+
 
     pygame.display.quit() # quit the GUI
         
--- src/lamp.py
+++ src/lamp.py
@@ -1,5 +1,5 @@
 import numpy as np
-
+import pygame
 
 boxWidth = 1000
 boxHeight = 800
@@ -9,23 +9,47 @@ blue = (0, 0, 128)
 # create lamp class which will be our creatures
 class lamp:
     def __init__(self, color):
-        self.x = np.random.randint(0, boxWidth)
-        self.y = np.random.randint(0, boxHeight)
-        self.xDot = np.random.randint(-5, 5)
-        self.yDot = np.random.randint(-5, 5)
-        ran = np.random.randint(-10,11)
-        if ( ran < 0 ):
-            self.color = color
-        else:
-            self.color= green
-
+        self.max_velocity = 3
+        
+        x = np.random.randint(0, boxWidth)
+        y = np.random.randint(0, boxHeight)
+        pos = [x,y]
+        self.position = np.array(pos)
+        
+        xDot = np.random.randint(-1, 2)
+        yDot = np.random.randint(-1, 2)
+        vel = [xDot,yDot]
+        self.velocity = np.array(vel)
+        
+        self.color = color
+        self.height = 9
+        self.length = 3
+        self.energy = 50
+        
     def move(self):
-        ran = np.random.randint(-10,11)
-        if (ran < 0):
-            self.x = self.x + self.xDot
-            self.xDot = np.random.randint(-10, 11)
-        elif(ran > 0):
-            self.y = self.y + self.yDot
-            self.yDot = np.random.randint(-10, 11)
+        x_force = np.random.randint(-1,2)
+        y_force = np.random.randint(-1,2) 
+        self.force = [x_force, y_force]
+        
+        if ((self.velocity[0] + self.force[0])**2 + (self.velocity[1] + self.force[1])**2)**(1/2) < self.max_velocity:
+            self.velocity += self.force
 
+        self.position += self.velocity
 
+        self.energy -= np.linalg.norm(self.velocity)/10
+        #if self.energy < 0:
+         #   print("YOU'RE DEAD, e = ", self.energy)
+
+class Food:
+    def __init__(self, heig, leng):
+        x = np.random.randint(0, boxWidth)
+        y = np.random.randint(0, boxHeight)
+        self.position = (x,y)
+        self.height = heig
+        self.length = leng
+
+    def respawn(self):
+        x = np.random.randint(0, boxWidth)
+        y = np.random.randint(0, boxHeight)
+        self.position = (x,y)
+        
