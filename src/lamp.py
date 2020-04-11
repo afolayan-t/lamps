41d4537550fc2582559c97e282407f71af1c2548
Salvatore Ferrone <sferrone@ithaca.edu>
2020-04-11 01:22:33 -0400

    Stink field value now assigned to lamp nostrils

--- src/lamp.py
+++ src/lamp.py
@@ -6,15 +6,28 @@ green = (0, 255, 0)
 blue = (0, 0, 128)
 red = (255,0, 0)
 
-res = 1 # resolution!
+res = 1 # grid resolution!
 grid_x = np.linspace(0, boxWidth, boxWidth)
 grid_y = np.linspace(0, boxHeight, boxHeight)
 (XS, YS) = np.meshgrid(grid_x,grid_y)
 
 dt = .5 # Time Step
 
+
+
 # create lamp class which will be our creatures
 class lamp:
+    ### ATTRIBUTES
+    #
+    #   Static attributes
+    #   length, height, maxSpeed, maxEnergy, color, _verticies, stinkRadius
+    #   
+    #   Dynamic attributes 
+    #   position, velocity, force, energy, verticies, scentPoints, stinkField, scentValues 
+
+    #   nomenclature:
+    #   scent is what you detect where stink is what you are
+
     def __init__(self, color=red, x=None, y=None, max_velo=3,
                  length=30, height=25):
         
@@ -45,8 +58,8 @@ class lamp:
         self.maxEnergy = self.length*self.height
         self.energy = self.maxEnergy/2
 
-        self.scentMagnitude = np.zeros([8,3]) # initialize array where we will smell stinky food
-        # Maybe we will need to store the previous scent as well? Not sure. Depends on learning algorithm
+        self.scentMagnitude = np.zeros([3,3]) # row is nostril, col is rgb
+        self.stinkRadius = 5
         self.setVertices()
         self.setScentPoints()
         
@@ -72,7 +85,25 @@ class lamp:
         self.position = self.position + self.velocity*dt
 
         ## rotate the lamp
-        self.rotate()
+        self.rotate() # also moves scent points
+
+        ## Smell the environment 
+
+
+        ## Move the sinkfield of the lamp
+        self.setStinkField()
+
+
+    def smell(self, globalStinkField):
+        # assign the globalStinkField's rgb values to each nostril
+
+        # allign coordiantes
+        for i in range(0, len(self.scentPoints)):
+            xMins = XS-self.scentPoints[i,0] # get minimum distance from grid point
+            yMins = YS-self.scentPoints[i,1] 
+            nearest = xMins**2 + yMins**2
+            nearestIndicies= np.where(nearest == np.amin(nearest)) # returns indicies
+            self.scentMagnitude[i,:] = globalStinkField[nearestIndicies[0], nearestIndicies[1], :]
 
 
 
@@ -81,7 +112,6 @@ class lamp:
     def rotate(self):
         # (1) Get unit vector of the velocity for direction
         # (2) Get the rotation matrix
-
 
         # GET UNIT VECTOR
         magV = np.linalg.norm(self.velocity)
@@ -112,7 +142,44 @@ class lamp:
             self.scentPoints = np.floor(self.scentPoints + self.position) # scent needs to be on the grid
 
 
+    
+    def setScentPoints(self):
+        ### Set the scent points around the lamp. 
+        ### The scent points will be length /2 from the lamps center
+        ### There will be N scent points
+        ### They will have three channels. 
+
+        ### I wonder the best way to store the values and positions? 
+        ### maybe the should be structures? OR dictionaries? 
+        ### I should be which ever is easiest to give to Tolu for reinforcement learning
+
+        ### set general scent points
+        nScentPoints = 3 # general scent points
+        nostralAngle = np.pi/3
+        theta = np.linspace(-nostralAngle, nostralAngle, nScentPoints)
+        r = (self.height)/2
+        scent_x = r*np.cos(theta)
+        scent_y = r*np.sin(theta)
+        tempCoordaintes = np.transpose(np.array([scent_x, scent_y])) # make each row be a coordinate
+
+        # Rotate each scent by -90 degrees to be coordiante with lamp
+        angle = 0# -np.pi/2
+        rotationMatrix = np.array([[np.cos(angle), -np.sin(angle)], [np.sin(angle), np.cos(angle)]])
+
+        self._scentPoints = np.floor(np.transpose(np.matmul(rotationMatrix, np.transpose(tempCoordaintes))))
+        # put us with the current position of the lamp
+        self.scentPoints = np.floor(np.add(self._scentPoints, self.position))
+
+    def setStinkField(self):
+        # define stink field as a XSxYSx3 array. I.E. an RGB at every coordinate
+        # a three dimensional stink field lol
+
+        self.stinkField = np.zeros(np.append(np.array(XS.shape), 3)) # intialize array
+        stinkPlane = np.exp(-(1/self.stinkRadius)*(( (XS-self.position[0])**2 + (YS-self.position[1])**2 ) ** (1/2)))
+        for i in range(0,3):
+            self.stinkField[:,:,i] =  self.color[i] * stinkPlane # magnitude of each scent is the color of the food
         
+
 
     def setVertices(self):
         ###                  _
@@ -136,35 +203,6 @@ class lamp:
         vertices = np.transpose(np.matmul(rotationMatrix, np.transpose(vertices)))
         self.vertices = np.add(vertices, self.position)
         self._vertices = vertices
-    
-    def setScentPoints(self):
-        ### Set the scent points around the lamp. 
-        ### The scent points will be length /2 from the lamps center
-        ### There will be N scent points
-        ### They will have three channels. 
-
-        ### I wonder the best way to store the values and positions? 
-        ### maybe the should be structures? OR dictionaries? 
-        ### I should be which ever is easiest to give to Tolu for reinforcement learning
-
-        ### set general scent points
-        nPoints = 3 # general scent points
-        nostralAngle = np.pi/3
-        theta = np.linspace(-nostralAngle, nostralAngle,nPoints)
-        r = (self.height)/2
-        scent_x = r*np.cos(theta)
-        scent_y = r*np.sin(theta)
-        tempCoordaintes = np.transpose(np.array([scent_x, scent_y])) # make each row be a coordinate
-
-        # Rotate each scent by -90 degrees to be coordiante with lamp
-        angle = 0# -np.pi/2
-        rotationMatrix = np.array([[np.cos(angle), -np.sin(angle)], [np.sin(angle), np.cos(angle)]])
-
-        self._scentPoints = np.floor(np.transpose(np.matmul(rotationMatrix, np.transpose(tempCoordaintes))))
-        # put us with the current position of the lamp
-        self.scentPoints = np.floor(np.add(self._scentPoints, self.position))
-
-
 
 
 class Food:
@@ -179,13 +217,24 @@ class Food:
         else:
             y_ = y
 
+        self.color = (209, 230, 69) # some ugly yellow
+        self.stinkRadius = 50 # set stink radius. How far until (1/e) is reached
+
         self.position = (x_,y_)
         self.height = height
         self.length = length
         self.energy = self.height*self.length/2
-        self.stinkMag = 5
-        self.stinkRadius = 50
-        self.stink = self.stinkMag*np.exp(-(1/self.stinkRadius)*(( (XS-self.position[0])**2 + (YS-self.position[1])**2 ) ** (1/2)))
+        self.setStinkField()
+
+    def setStinkField(self):
+        # define stink field as a XSxYSx3 array. I.E. an RGB at every coordinate
+        # a three dimensional stink field lol
+
+        self.stinkField = np.zeros(np.append(np.array(XS.shape), 3)) # intialize array
+        stinkPlane = np.exp(-(1/self.stinkRadius)*(( (XS-self.position[0])**2 + (YS-self.position[1])**2 ) ** (1/2)))
+        for i in range(0,3):
+            self.stinkField[:,:,i] =  self.color[i] * stinkPlane # magnitude of each scent is the color of the food
+
 
 
 
@@ -195,19 +244,23 @@ class Food:
         self.position = (x,y)
 
 
-# food_colony = []
-# totalStink = np.zeros([boxHeight,boxWidth])
-# for i in range(0, 10):
-#     food_i = Food()
-#     food_colony.append(food_i)    
-#     totalStink = totalStink + food_colony[i].stink
+food_colony = []
+totalStink = np.zeros([boxHeight,boxWidth, 3])
+for i in range(0, 10):
+    food_i = Food()
+    food_colony.append(food_i)    
+    totalStink = totalStink + food_colony[i].stinkField
+
+np.sum(food_colony[:].totalStink)
+myLamp = lamp()
+myLamp.move()
+myLamp.smell(totalStink)
 
 # fig,ax=plt.subplots(1,1)
 
-
-# cp = ax.contourf(XS, YS, totalStink)
+# cp = ax.contourf(XS, YS, totalStink[:,:,1])
 # fig.colorbar(cp) # Add a colorbar to a plot
 # ax.set_title('Filled Contours Plot')
-# #ax.set_xlabel('x (cm)')
+# ax.set_xlabel('x (cm)')
 # ax.set_ylabel('y (cm)')
 # plt.show()
